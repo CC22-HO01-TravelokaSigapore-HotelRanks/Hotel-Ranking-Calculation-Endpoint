eabeffb600cf5dec3fd042bc308ccd577bff6625
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-05-18 17:11:38 +0700

    Barely working hotel similarity no ids checking and others

--- api.py
+++ api.py
@@ -2,9 +2,12 @@
 from database_cache import db
 
 # For API stuff
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 import pandas as pd
 import numpy as np
+
+# Hotel Similarity
+import modules.hotel_similarity as similarity_mods
 
 router = APIRouter()
 
@@ -12,9 +15,48 @@ router = APIRouter()
 async def hello_world():
     return "Hello from hotel ranking calculation endpoint"
 
+@router.post("/re-cached/")
+async def re_cached_db(response: Response):
+    try:
+        db.update_df()
+    except Exception as e:
+        print(e)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return "Cannot re-cached the server"
+
 @router.get("/rows-cached/")
-async def hotel_ranking():
-    dfs_rows = {}
-    for i in db.df:
-        dfs_rows[i] = len(db.get_dataframe(i))
-    return dfs_rows
+async def hotel_ranking(response: Response):
+    try:
+        dfs_rows = {}
+        for i in db.df:
+            dfs_rows[i] = len(db.get_dataframe(i))
+        return dfs_rows
+    except Exception as e:
+        print(e)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return "Internal server error"
+
+@router.post("/hotel-similarity/{hotel_id}")
+async def hotel_similar(hotel_id: int, response: Response):
+    """
+    An API for calculating similarity between hotel of interest and the other
+    hotel using cosine similarity. 
+    """
+    try:
+        df = db.get_dataframe("hotels").copy()
+        
+        # # Check if hotel of interest is in the dataframes
+        # # WARNING NO ID CHECKING
+        # if not similarity_mods.id_is_available(hotel_id, df):
+        #     response.status_code = status.HTTP_400_BAD_REQUEST
+        #     return "Hotel ID is not found/cached yet in ML. Please do recached on POST /re-cached/"
+        
+        # Calculate cosine similarity and give recommendation
+        # TODO: Convert ids to idx
+        idx_recommendation = similarity_mods.hotel_similarity(hotel_id, 10, df)
+        # TODO: Convert idx recommendation to ids
+        return idx_recommendation.tolist()
+    except Exception as e:
+        print(e)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return "Internal server error"
---
+++ modules/hotel_similarity.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from typing import List
+
+# Min-max normalization
+def min_max_normalization(arr:List) -> List:
+  minimum = min(arr)
+  maximum = max(arr)
+
+  if (maximum - minimum) == 0:
+    return arr
+
+  for i in range(len(arr)):
+    arr[i] = (arr[i] - minimum) / (maximum - minimum)
+  return arr
+
+# Preprocess df
+def preprocess_dataframe(df_in: pd.DataFrame) -> pd.DataFrame:
+  df = df_in.copy()
+  column_not_processed = ["latitude", "longitude"]
+
+  for i in df.columns:
+    current_list = df[i].to_list()
+
+    if i in column_not_processed:
+      continue
+    
+    if df[i].dtypes in ["float64", "int64"]:
+      df[i] = min_max_normalization(current_list)
+    
+    if df[i].dtypes == "bool":
+      df[i] = df[i].astype(int)
+
+  return df
+
+def index_to_id(id: int, df: pd.DataFrame) -> int:
+  return df.iloc[1]["id"]
+
+def id_to_index(id: int, df: pd.DataFrame) -> int:
+  return df[df["id"] == id].index[0]
+
+def id_is_available(id: int, df: pd.DataFrame) -> bool:
+  return  id in df["id"].to_list()
+
+def hotel_similarity(idx:int, num_recs:int, df:pd.DataFrame) -> np.ndarray:
+  df_att = df.copy().drop(columns=["name", "neighborhood", "type_nearby_destination", "image_links"])
+  df_att_norm = preprocess_dataframe(df_att)
+  df_att_norm.drop(columns=["latitude", "longitude"], inplace=True)
+  df_att_norm.dropna(inplace=True)
+  pivoted = df_att_norm.iloc[idx].to_list()
+  similarity = cosine_similarity(df_att_norm.values.tolist(), np.asarray(pivoted).reshape(1,-1)).reshape((1,-1))[0]
+  sorted = np.argsort(similarity)[::-1][:num_recs+1]
+  sorted_wo_first = sorted[1:]
+  return sorted_wo_first
