d2022a4d5b35b126f9a3fb41c8ab258262e55cfb
Farel Arden <farelarden13@gmail.com>
2022-06-03 16:16:47 +0700

    Add files via upload

--- modules/near_you/api.py
+++ modules/near_you/api.py
@@ -4,21 +4,28 @@ import traceback
 from database_cache import db
 
 # For API stuff
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response, status, Request
 
 # Hotel similarity function
 from modules.near_you.function import *
 
 near_you_router = APIRouter()
-
-@near_you_router.post("/near_you/{coordinate}")
-async def sentiment_similarity(coordinate: tuple, response: Response):
+@near_you_router.post("/near_you/")
+async def sentiment_similarity(request: Request, response: Response):
     """
     An API for giving recommendation with weighted sentiment 
     """
     try:        
         df_hotels = db.get_dataframe("hotels").copy()
-        user_coor = (-8.685924978406074, 115.16562524113574)
+        json_req = await request.json()
+        latitude = json_req["latitude"]
+        longitude = json_req["longitude"]
+
+        if not (type(latitude) == float and type(longitude) == float):
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return "Position is not found/cached yet in ML. Please do recached on POST /re-cached/"
+
+        user_coor = (latitude, longitude)
         return near_you(user_coor,df_hotels)
     except Exception as e:
         traceback.print_exc()
